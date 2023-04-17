35f924aac1fc458707eebbd5199f00d180d5179a
talhz <talhz@pku.edu.cn>
2023-04-17 12:03:48 -0400

    A new nonconvex example

---
+++ Projected_GD/nonconvex2.py
@@ -0,0 +1,27 @@
+import torch
+from general_gd import GeneralGD
+
+
+
+## Example: Thompson Problem    
+
+# Define the quadratic function to minimize.
+def f(X):
+    n = X.shape[0]
+    energy = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            dist = -torch.norm(X[i,:] - X[j,:])**2
+            energy += dist
+    return energy*2
+
+# Generate some random data for X0.
+k, n = 3, 5
+X0 = torch.randn(n, k)
+
+learner = GeneralGD(f, X0)
+X_opt, f_val, n_iters = learner.train()
+learner.result()
+
+# 3d plot
+learner.plotX()
