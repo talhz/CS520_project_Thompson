519fb76da57a794767feb23cd2f1eb3b9c29f814
talhz <talhz@pku.edu.cn>
2023-04-16 22:28:46 -0400

    Thompson problem with L1 norm

---
+++ Thompson_L1.py
@@ -0,0 +1,25 @@
+import torch
+from general_gd import GeneralGD
+
+## Example: Thompson Problem    
+
+# Define the quadratic function to minimize.
+def f(X):
+    n = X.shape[0]
+    energy = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            dist = torch.norm(X[i,:] - X[j,:])
+            energy += 1 / dist
+    return energy*2
+
+# Generate some random data for X0.
+k, n = 3, 10
+X0 = torch.randn(n, k)
+
+learner = GeneralGD(f, X0)
+X_opt, f_val, n_iters = learner.train()
+learner.result()
+
+# 3d plot
+learner.plotX()
