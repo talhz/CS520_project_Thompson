6d95afa08e3ca5e23317561534598966de7ea8a8
talhz <talhz@pku.edu.cn>
2023-04-28 20:55:50 -0400

    fix augmented Lagrange method

--- Projected_GD/augmented_Lagrange.py
+++ Projected_GD/augmented_Lagrange.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Augmented_Lagragian:
-    def __init__(self, f, X0, lr=1e-5, steps=1000, max_iters=100, tol=1e-7):
+    def __init__(self, f, X0, lr=1e-3, steps=20, max_iters=100, tol=1e-7):
         """
         Initialization of augmented Lagragian algorithm 
         
@@ -27,58 +27,51 @@ class Augmented_Lagragian:
         n = X.shape[0]
         sum = 0
         for i in range(n):
-            sum += 0.5 * Mu * (torch.norm(X[i])**2 - 1)**2 + Lambda[i] * (torch.norm(X[i])**2 - 1)
+            sum += 0.5 * Mu * (torch.norm(X[i])**2 - 1)**2 - Lambda[i] * (torch.norm(X[i])**2 - 1)
         return self.f(X) + sum
     
-    # def closure(self):
-    #     loss = self.objective(self.X, self.Lambda, self.Mu)
-    #     loss.backward()
-    #     return loss
+    def constraint(self, X):
+        """
+        Define the constraint: diag(X*X^T) = e.
+        """
+        return torch.norm(torch.diagonal(self.X.detach() @ self.X.detach().t()) - torch.ones(self.X.detach().shape[0]), p=2)
     
     def train(self):
-        self.X = self.X0.clone().requires_grad_(True)
+        self.X = self.X0.clone().double().requires_grad_(True)
         self.step = 0
         n = self.X.shape[0]
         self.Lambda = [1 for i in range(n)]
-        self.Mu = 1
+        self.Mu = 10
         self.f_val = self.objective(self.X, self.Lambda, self.Mu)
         while self.step < self.steps:
-            # self.optimizer = torch.optim.Adam([self.X], lr=self.lr)
-            # self.n_iters = 0
-            # while self.n_iters < self.max_iters:
-            #     loss = self.objective(self.X, self.Lambda, self.Mu)
-            #     self.optimizer.zero_grad()
-            #     loss.backward(retain_graph=True)
-            #     self.optimizer.step()
-            #     self.n_iters += 1
             self.n_iters = 0
             while self.n_iters < self.max_iters:
             # Compute the gradient of f(X) w.r.t. X.
                 grad_f = torch.autograd.grad(self.f_val, self.X)[0]
-                # Check gradient
-                # torch.autograd.gradcheck(self.objective, inputs=[self.X, Lambda, Mu], eps=1e-2, atol=1e-2)
+                # Check gradient TODO: only check gradient for certain iteration. 
+                # torch.autograd.gradcheck(self.objective, inputs=[self.X, self.Lambda, self.Mu], eps=1e-4, atol=1e-2)
                 self.X = self.X - self.lr * grad_f
                 # Update the function value and check for convergence.
                 f_old = self.f_val
-                self.f_val = self.f(self.X)
-                if torch.abs((self.f_val - f_old) / f_old) < self.tol:
+                self.f_val = self.objective(self.X, self.Lambda, self.Mu)
+                if torch.norm(grad_f[0])**2 < self.tol:
                     break
                 self.n_iters += 1
-            # print(self.n_iters)
             for i in range(len(self.Lambda)):
-                self.Lambda[i] += self.Mu * (torch.norm(self.X[i])**2 - 1)
+                self.Lambda[i] = self.Lambda[i] - self.Mu * (torch.norm(self.X[i])**2 - 1)
             self.Mu += 0.1
             self.step += 1
+            print(self.f_val)
         return self.X.detach(), self.f_val.detach(), self.step
     
     def result(self):
         
         print("Optimal solution X:\n", self.X.detach())
-        print("Value of f(X) at the optimal solution:", self.f_val.detach())
+        print("Value of f(X) at the optimal solution:", self.f(self.X))
         print("Number of iterations until convergence:", self.step)
         print("Mu", self.Mu)
         print("Lambda", self.Lambda)
-        # print("Constraint violation at the optimal solution:", self.constraint(self.X.detach()))
+        print("Constraint violation at the optimal solution:", self.constraint(self.X.detach()))
         
     def plotX(self, save=False):
         """
@@ -110,21 +103,23 @@ class Augmented_Lagragian:
 ## Example: Thompson Problem    
 
 # Define the quadratic function to minimize.
-def f(X):
-    n = X.shape[0]
-    energy = 0
-    for i in range(n):
-        for j in range(i+1, n):
-            dist = torch.norm(X[i,:] - X[j,:])**2
-            energy += 1 / dist
-    return energy*2
+if __name__ == "__main__":
+    def f(X):
+        n = X.shape[0]
+        energy = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                dist = torch.norm(X[i,:] - X[j,:])
+                energy += 1 / dist
+        return energy
 
-# Generate some random data for X0.
-k, n = 3, 10
-X0 = torch.randn(n, k)
-learner = Augmented_Lagragian(f, X0)
-X_opt, f_val, n_iters = learner.train()
-learner.result()
+    # Generate some random data for X0.
+    k, n = 3, 30
+    X0 = torch.randn(n, k)
+    learner = Augmented_Lagragian(f, X0)
+    X_opt, f_val, n_iters = learner.train()
+    learner.result()
 
-# 3d plot
-learner.plotX()
+    # 3d plot
+    if k == 3:
+        learner.plotX()
